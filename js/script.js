9767ff5fa317f27b380fee394cb62fc9de18f9f9
ArpaSaha-AS <85447920+ArpaSaha-AS@users.noreply.github.com>
2021-09-09 10:17:27 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//Answer To The Question Number:6 start
+function myFunction() {
+    document.getElementById("demo6").innerHTML = "Paragraph changed.";
+}
+//Answer To The Question Number:7 start
+function myFunction() {
+    document.getElementById("demo7").innerHTML = "Paragraph changed.";
+}
+//Answer To The Question Number:8 start
+document.getElementById("demo8").innerHTML = 5 + 6;
+//Answer To The Question Number:10 start
+window.alert(5 + 6);
+//Answer To The Question Number:12 start
+let x, y, z; // Statement 1
+x = 5; // Statement 2
+y = 6; // Statement 3
+z = x + y; // Statement 4
+
+document.getElementById("demo12").innerHTML = "The value of z is " + z + ".";
+//Answer To The Question Number:13 start
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+document.getElementById("demo13").innerHTML = c;
+//Answer To The Question Number:14 start
+function myFunction() {
+    document.getElementById("demo14").innerHTML = "Hello Dolly!";
+    document.getElementById("demo144").innerHTML = "How are you?";
+}
+//Answer To The Question Number:15 start
+document.getElementById("demo15").innerHTML = 10.5;
+//Answer To The Question Number:16 start
+let x16;
+x16 = 6;
+document.getElementById("demo16").innerHTML = x16;
+//Answer To The Question Number:17 start
+let lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+document.getElementById("demo17").innerHTML = lastName;
+//Answer To The Question Number:21 start
+let p = 5;
+let q = 2;
+let r = p + q;
+document.getElementById("demo21").innerHTML = r;
+//Answer To The Question Number:69 start
